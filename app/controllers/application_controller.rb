4974d0905140d702786b64d702ba1b2c02833a64
juniorfigti5 <wr.guarin234@uniandes.edu.co>
2018-01-27 15:46:36 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+    render html: "Hola, world!"
+  end
+  
+  def goodbye
+    render html: "bye, world!"
+  end
+end
